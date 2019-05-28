4bd1c3b94cfe83fd9c36f0cf21147226a8092ae9
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2019-05-28 18:49:26 +0700

    add first impl

---
+++ packed/_resolver.py
@@ -0,0 +1,27 @@
+from typing import Any, Dict, Optional, Type
+
+__all__ = ("Resolver",)
+
+
+AnyClass = Type[Any]
+
+
+class Resolver:
+    def __init__(self) -> None:
+        self._registry: Dict[str, AnyClass] = {}
+
+    def register(self, cls: AnyClass, name: Optional[str] = None) -> None:
+        if name is None:
+            name = cls.__name__
+        self._registry[name] = cls
+
+    def resolve_type(self, name: str) -> AnyClass:
+        if name not in self._registry:
+            raise KeyError(name)
+        return self._registry[name]
+
+    def resolve_name(self, cls: AnyClass) -> str:
+        for key, val in self._registry.items():
+            if val == cls:
+                return key
+        raise KeyError(cls)
