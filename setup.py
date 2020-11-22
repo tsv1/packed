4af91984c3cc6530bf18a8eb343abcc563aee597
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2020-11-22 16:42:20 +0700

    bump version → 0.1.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="packed",
-    version="0.1.2",
+    version="0.1.3",
     description="",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
