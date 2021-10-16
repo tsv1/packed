a26cc775b3552874f0c3941498340feadd4ee695
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2021-10-16 20:57:01 +0700

    update packages

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
     url="https://github.com/nikitanovosibirsk/packed",
     license="Apache-2.0",
     packages=find_packages(exclude=("tests",)),
+    package_data={"packed": ["py.typed"]},
     install_requires=find_required(),
     tests_require=find_dev_required(),
     classifiers=[
@@ -31,5 +32,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Typing :: Typed",
     ],
 )
