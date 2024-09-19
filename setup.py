6bca8c0951d8b0d5e4748b18627a624cf49e5f1c
Nikita Tsvetkov <tsv1@fastmail.com>
2024-09-19 20:08:32 +0400

    bump version → 0.3.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="packed",
-    version="0.2.3",
+    version="0.3.0",
     description="Simple serialization and deserialization of custom Python objects using MessagePack",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
